900acb1dd2884aaba1c00f01449dc24b24f8cee1
Junior Klegseth <jklegseth@gmail.com>
2018-03-15 20:29:54 -0700

    overhauled packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,66 @@
-'use strict';
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/main.js',
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-  devtool: 'source-map',
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}
 
-  output: {
-    filename: './dist/bundle.js'
-  },
+module.exports = env => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+  return {
+    entry: path.join(__dirname, 'src/main.js'),
+    devtool: 'cheap-eval-source-map',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.json'],
+    },
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist',
+    },
+  };
 };
